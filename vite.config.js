dbbac82a5a54c72e349af98049e8e228b966f390
David <david.tomi2020@gmail.com>
2025-05-22 02:43:40 +0100

    feat:fixed directory bug

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
+  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
