55f9beb5832b77186dde25b56e6fc9125d0243ca
David <david.tomi2020@gmail.com>
2025-05-23 12:50:42 +0100

    fix

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -119,7 +119,7 @@ export const Search = () => {
         </div>
 
         {/* Filtered results */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6 md:gap-8 p-2 md:p-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6 md:gap-8 p-2 md:p-4">
           {filterItems(product.products)}
         </div>
 
